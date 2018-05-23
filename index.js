0afc49d78d45187b4ebbe2623cc0cb02fdbfcac7
rbung <rkbung@gmail.com>
2018-05-23 16:16:39 +0200

    first version

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-'use strict';
+'use strict'
 
-const data = require('./data.js');
-const args = process.argv;
+const { endpoints, keys } = require('./data')
+const values = Object.values(keys)
+const args = process.argv
 
-if(args.length > 2){
-  let endpoints = args.slice(2, args.length);
-  for(let i in endpoints){
-    console.log(getSecretKey(endpoints[i]));
-  };
-}
-else{
-  console.log('you must specify an endpoint');
-  process.exit(0);
+const getSecretKey = function(endpoint) {
+  const index = endpoints.indexOf(endpoint)
+  if (index === -1) {
+    return 'ERROR'
+  }
+  return values[index]
 }
 
-const getSecretKey = function(endpoint){
-  //TODO
-};
-
-
-
-
-
-
+if (args.length > 2) {
+  let endpoints = args.slice(2, args.length)
+  for (let i in endpoints) {
+    console.log(getSecretKey(endpoints[i]))
+  }
+} else {
+  console.log('you must specify an endpoint')
+  process.exit(0)
+}
